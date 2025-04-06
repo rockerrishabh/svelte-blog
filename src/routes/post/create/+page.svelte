e2d74c852fbd69a8310ae4a06c750ccb6095b67c
Rishabh Kumar <rishabh.6542@gmail.com>
2025-04-06 14:22:51 +0530

    removed old files

--- src/routes/post/create/+page.svelte
+++ src/routes/post/create/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import Editor from '../../../components/Editor.svelte';
+	import Editor from '../../../components/Editor/Editor.svelte';
 	import type { PageProps } from './$types';
 	let { form }: PageProps = $props();
 </script>
